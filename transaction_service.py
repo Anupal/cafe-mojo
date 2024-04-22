a1597f4306ea084550707d8dc717dd99c6930159
Anupal Mishra <anupal.pm@gmail.com>
2024-04-22 02:01:10 +0100

    separate service files

---
+++ transaction_service.py
@@ -0,0 +1,7 @@
+from api.transaction import app
+import database.query as query
+
+
+if __name__ == "__main__":
+    app.config.update({"db_query": query})
+    app.run(host="0.0.0.0")
---
+++ user_service.py
@@ -0,0 +1,7 @@
+from api.user import app
+import database.query as query
+
+
+if __name__ == "__main__":
+    app.config.update({"db_query": query})
+    app.run(host="0.0.0.0")
