1202ada828d9b2d866466f1907c61d9ad6403674
Noêl Yves Patrick Mathis <mathisn@tcd.ie>
2024-04-22 03:00:40 +0100

    cli wip

--- cli/api_client.py
+++ cli/api_client.py
@@ -1,85 +1,88 @@
 import requests
 
+
 class APIClient:
-    BASE_URL = "http://127.0.0.1:5000"
+    def __init__(self, transaction_service_url="http://127.0.0.1:5000", user_info_service_url="http://127.0.0.1:5000"):
+        self.transaction_service_url = transaction_service_url
+        self.user_info_service_url = user_info_service_url
+
     access_token = None
 
-    @classmethod
-    def set_access_token(cls, token):
-        cls.access_token = token
+    def set_access_token(self, token):
+        self.access_token = token
 
-    @classmethod
-    def signup(cls, username, password):
-        response = requests.post(f"{APIClient.BASE_URL}/user/signup", json={"username": username, "password": password})
+    def signup(self, username, password):
+        response = requests.post(f"{self.user_info_service_url}/user/signup",
+                                 json={"username": username, "password": password})
         return response
 
-    @classmethod
-    def login(cls, username, password):
-        response = requests.post(f"{APIClient.BASE_URL}/user/login", json={"username": username, "password": password})
+    def login(self, username, password):
+        response = requests.post(f"{self.user_info_service_url}/user/login",
+                                 json={"username": username, "password": password})
         if response.status_code == 200:
-            cls.access_token = response.json().get('access_token')
+            self.access_token = response.json().get('access_token')
         return response
 
-    @classmethod
-    def get_authenticated_header(cls):
-        return {"Authorization": f"Bearer {cls.access_token}"} if cls.access_token else {}
+    def get_authenticated_header(self):
+        return {"Authorization": f"Bearer {self.access_token}"} if self.access_token else {}
 
-    @classmethod
-    def get_user_memberships(cls):
+    def get_user_memberships(self):
         """Get the community information of the currently logged-in user"""
-        headers = cls.get_authenticated_header()
-        response = requests.get(f"{cls.BASE_URL}/user/memberships", headers=headers)
+        headers = self.get_authenticated_header()
+        response = requests.get(f"{self.user_info_service_url}/user/memberships", headers=headers)
         return response
 
-    @classmethod
-    def create_group(cls, group_name):
+    def create_group(self, group_name):
         """Create a new group"""
-        headers = cls.get_authenticated_header()
+        headers = self.get_authenticated_header()
         data = {"name": group_name}
-        response = requests.post(f"{cls.BASE_URL}/group/add", headers=headers, json=data)
+        response = requests.post(f"{self.user_info_service_url}/group/add", headers=headers, json=data)
         return response
 
-    @classmethod
-    def add_member_to_group(cls, group_id, user_name):
+    def add_member_to_group(self, group_id, user_name):
         """Add user to group"""
-        headers = cls.get_authenticated_header()
+        headers = self.get_authenticated_header()
         data = {"group_id": group_id, "user_name": user_name}
-        response = requests.post(f"{cls.BASE_URL}/group/add-member", headers=headers, json=data)
+        response = requests.post(f"{self.user_info_service_url}/group/add-member", headers=headers, json=data)
         return response
 
-    @classmethod
-    def get_group_details(cls, group_id):
+    def get_group_details(self, group_id):
         """Get the details of the specified group."""
-        headers = cls.get_authenticated_header()
-        response = requests.get(f"{cls.BASE_URL}/group/{group_id}", headers=headers)
+        headers = self.get_authenticated_header()
+        response = requests.get(f"{self.user_info_service_url}/group/{group_id}", headers=headers)
         return response
 
-    @classmethod
-    def get_items(cls):
+    def get_items(self):
         """Get a list of all items"""
-        headers = cls.get_authenticated_header()
-        response = requests.get(f"{cls.BASE_URL}/item/all", headers=headers)
+        headers = self.get_authenticated_header()
+        response = requests.get(f"{self.transaction_service_url}/item/all", headers=headers)
         return response
 
-    @classmethod
-    def add_item(cls, name, price):
+    def add_item(self, name, price):
         """Adds a new item to the inventory"""
-        headers = cls.get_authenticated_header()
+        headers = self.get_authenticated_header()
         data = {
             "name": name,
             "price": price
         }
-        response = requests.post(f"{cls.BASE_URL}/item/add", headers=headers, json=data)
+        response = requests.post(f"{self.transaction_service_url}/item/add", headers=headers, json=data)
         return response
 
-    @classmethod
-    def create_transaction(cls, group_id, store, points_redeemed, items):
-        headers = cls.get_authenticated_header()
+    def create_transaction(self, group_id, store, points_redeemed, items):
+        headers = self.get_authenticated_header()
         data = {
             "group_id": group_id,
             "store": store,
             "points_redeemed": points_redeemed,
             "items": items
         }
-        response = requests.post(f"{cls.BASE_URL}/transaction/add", headers=headers, json=data)
+        response = requests.post(f"{self.transaction_service_url}/transaction/add", headers=headers, json=data)
         return response
+
+    def are_services_healthy(self):
+        try:
+            response_transaction_service = requests.get(f"{self.transaction_service_url}/healthcheck")
+            response_user_info_service = requests.get(f"{self.user_info_service_url}/healthcheck")
+        except requests.exceptions.ConnectionError:
+            return False
+        return response_transaction_service.status_code == 200 and response_user_info_service.status_code == 200
